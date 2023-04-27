991f7f3c461298785a603b3e1cca69ae9a26817b
Fab <fabiomiguelchenhuang@gmail.com>
2023-04-27 16:09:42 +0100

    feat: inventory persitent storage

--- Assets/Scripts/Inventory/Scripts/InventoryObject.cs
+++ Assets/Scripts/Inventory/Scripts/InventoryObject.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventor")]
-public class InventoryObject : ScriptableObject
+public class InventoryObject : ScriptableObject, ISerializationCallbackReceiver
 {
+    public string savePath;
+    private ItemDatabaseObject database;
     public List<InventorySlot> Container = new List<InventorySlot>();
     public void AddItem(ItemObject _item, int _amount)
     {
@@ -16,7 +21,49 @@ public class InventoryObject : ScriptableObject
                 return;
             }
         }
-            Container.Add(new InventorySlot(_item, _amount));
+        Container.Add(new InventorySlot(database.GetId[_item],_item, _amount));
+    }
+
+    public void OnAfterDeserialize()
+    {
+        for (int i = 0; i < Container.Count; i++)
+        {
+            Container[i].item = database.GetItem[Container[i].id];
+        }
+    }
+
+    public void OnBeforeSerialize()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    private void OnEnable()
+    {
+#if UNITY_EDITOR
+        database = (ItemDatabaseObject)AssetDatabase.LoadAssetAtPath("Assets/Resources/Database.asset", typeof(ItemDatabaseObject));
+#else
+        database = Resources.Load<ItemDatabaseObject>("Database");
+#endif
+    }
+
+    public void Save()
+    {
+        string saveData = JsonUtility.ToJson(this, true);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(string.Concat(Application.persistentDataPath, savePath));
+        bf.Serialize(file, saveData);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if(File.Exists(string.Concat(Application.persistentDataPath, savePath)))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(string.Concat(Application.persistentDataPath, savePath), FileMode.Open);
+            JsonUtility.FromJsonOverwrite(bf.Deserialize(file).ToString(), this);
+            file.Close();
+        }
     }
 }
 
@@ -25,8 +72,10 @@ public class InventorySlot
 {
     public ItemObject item;
     public int amount;
-    public InventorySlot(ItemObject _item, int _amount)
+    public int id;
+    public InventorySlot(int _id,ItemObject _item, int _amount)
     {
+        id = _id;
         item = _item;
         amount = _amount;
     }
