d8af0788838b0cdebee4710f674351b02ed9c2b5
Dudugs <duartesardao@sapo.pt>
2023-05-08 21:58:40 +0100

    fix bubble end

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -180,7 +180,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void JetPackParticles()
     {
-        if (velocityY < jetStrength / 10)
+        if (velocityY < jetStrength)
         {
             CancelInvoke(nameof(JetPackParticles));
             return;
