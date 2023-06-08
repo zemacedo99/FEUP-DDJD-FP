366faa3a7a0130f4cbf6d6d527ebe327ec370cb5
Fab <fabiomiguelchenhuang@gmail.com>
2023-06-08 16:34:00 +0100

    fix: reset puzzle problem

--- Assets/Scripts/PuzzlePauseMenuScript.cs
+++ Assets/Scripts/PuzzlePauseMenuScript.cs
@@ -65,20 +65,11 @@ public class PuzzlePauseMenuScript : MonoBehaviour
 
     public void ResetQuest()
     {
-        
-        SceneManager.LoadScene("Puzzle");
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     public void AbandonQuest()
     {
-        print("Leaving");
         SceneManager.LoadScene("World");
-        //GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<PlayerInventory>().inventory.Load(); // Load Player Current 
-
-        //// TODO: Spawn Player to to corresponding spawning state
-        //int currentSpawnPoint = PlayerPrefs.GetInt("SPAWN_POINT");
-
-        //print("Spawn point = " + currentSpawnPoint);
     }
 
     void ExecuteSelectedOption()
