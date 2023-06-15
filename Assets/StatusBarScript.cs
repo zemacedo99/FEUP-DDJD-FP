67955a18539b1136fd2a52d8e2c15bcf46904aa0
Fab <fabiomiguelchenhuang@gmail.com>
2023-06-16 00:25:00 +0100

    feat: red warning color when oxygen reaches less than 20%

--- Assets/StatusBarScript.cs
+++ Assets/StatusBarScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -8,7 +6,9 @@ public class StatusBarScript : MonoBehaviour
 {
     GameObject StatusBar;
     public float currentFilledPercentage;
-    // Start is called before the first frame update
+    public Color healthyColor;
+    public Color dangerColor;
+
     void Start()
     {
         StatusBar = GameObject.Find("StatusBarCircle");
@@ -18,18 +18,37 @@ public class StatusBarScript : MonoBehaviour
 
     public void UpdateFilledAmount(float value)
     {
-        if(value < 0f || value > 1f)
+        if(value < 0f )
         {
             return;
         }
-        StatusBar.transform.Find("Fill").GetComponent<Image>().fillAmount = value;
+        if (value > 1)
+        {
+            value = 1;
+        }
 
-        StatusBar.transform.Find("TextValue").GetComponent<TextMeshProUGUI>().text = ((int)(value*100)).ToString() + "%";
+        currentFilledPercentage = value;
+
+        UpdateStatusBarColor();
+
+        StatusBar.transform.Find("Fill").GetComponent<Image>().fillAmount = currentFilledPercentage;
+
+        StatusBar.transform.Find("TextValue").GetComponent<TextMeshProUGUI>().text = ((int)(currentFilledPercentage * 100)).ToString() + "%";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateStatusBarColor()
     {
-        UpdateFilledAmount(currentFilledPercentage);
+        Color color = healthyColor;
+
+        if (currentFilledPercentage < 0.21f)
+        {
+            color = dangerColor;
+        }
+        GameObject.Find("UI").GetComponent<Image>().color = color;
+        StatusBar.transform.Find("Fill").GetComponent<Image>().color = color;
+        StatusBar.transform.Find("TextValue").GetComponent<TextMeshProUGUI>().color = color;
+        StatusBar.transform.Find("TextValue").GetComponent<TextMeshProUGUI>().outlineColor = color == healthyColor ? new Color (159, 234,227) : color;
+
     }
+
 }
