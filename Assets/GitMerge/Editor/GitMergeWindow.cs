8d9e3b57dde8e18235ab3c5cf14e1ad54e059268
Dudugs <duartesardao@sapo.pt>
2023-06-18 09:56:32 +0100

    clone gitmerge

---
+++ Assets/GitMerge/Editor/GitMergeWindow.cs
@@ -0,0 +1,361 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using System.Collections.Generic;
+using GitMerge.Utilities;
+
+namespace GitMerge
+{
+    /// <summary>
+    /// The window that lets you perform merges on scenes and prefabs.
+    /// </summary>
+    public class GitMergeWindow : EditorWindow
+    {
+        private VCS vcs = new VCSGit();
+        
+        private const string EDITOR_PREFS_AUTOMERGE = "GitMerge_automerge";
+        private const string EDITOR_PREFS_AUTOFOCUS = "GitMerge_autofocus";
+        
+        public static bool automerge { private set; get; }
+        public static bool autofocus { private set; get; }
+        
+        private MergeManagerBase mergeManager;
+
+        private MergeFilter filter = new MergeFilter();
+        private MergeFilterBar filterBar = new MergeFilterBar();
+
+        public bool mergeInProgress => mergeManager != null;
+
+        private PageView pageView = new PageView();
+        private Vector2 scrollPosition = Vector2.zero;
+        private int tab = 0;
+        private List<GameObjectMergeActions> mergeActionsFiltered;
+
+        [MenuItem("Window/GitMerge")]
+        static void OpenEditor()
+        {
+            var window = EditorWindow.GetWindow(typeof(GitMergeWindow), false, "GitMerge");
+            // In case we're merging and the scene becomes edited,
+            // the shown SerializedProperties should be repainted
+            window.autoRepaintOnSceneChange = true;
+            window.minSize = new Vector2(500, 100);
+        }
+
+        private void OnEnable()
+        {
+            pageView.NumElementsPerPage = 200;
+            filterBar.filter = filter;
+            filter.OnChanged += CacheMergeActions;
+            LoadSettings();
+        }
+
+        private static void LoadSettings()
+        {
+            automerge = EditorPrefs.GetBool(EDITOR_PREFS_AUTOMERGE, true);
+            autofocus = EditorPrefs.GetBool(EDITOR_PREFS_AUTOFOCUS, true);
+        }
+
+        void OnHierarchyChange()
+        {
+            // Repaint if we changed the scene
+            this.Repaint();
+        }
+
+        // Always check for editor state changes, and abort the active merge process if needed
+        private void Update()
+        {
+            if (MergeAction.inMergePhase &&
+                (EditorApplication.isCompiling ||
+                 EditorApplication.isPlayingOrWillChangePlaymode))
+            {
+                ShowNotification(new GUIContent("Aborting merge due to editor state change."));
+                AbortMerge(false);
+            }
+        }
+
+        private void AbortMerge(bool showNotification = true)
+        {
+            mergeManager.AbortMerge(showNotification);
+            mergeManager = null;
+        }
+
+        private void OnGUI()
+        {
+            Resources.DrawLogo();
+            DrawTabButtons();
+            switch (tab)
+            {
+                case 0:
+                    OnGUIStartMergeTab();
+                    break;
+
+                default:
+                    OnGUISettingsTab();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Tab that offers scene merging.
+        /// </summary>
+        private void OnGUIStartMergeTab()
+        {
+            if (!mergeInProgress)
+            {
+                DisplayPrefabMergeField();
+                GUILayout.Space(20);
+                DisplaySceneMergeButton();
+            }
+            else
+            {
+                DisplayMergeProcess();
+            }
+        }
+
+        private void DisplaySceneMergeButton()
+        {
+            var activeScene = SceneManager.GetActiveScene();
+
+            GUILayout.Label("Open Scene: " + activeScene.path);
+            if (activeScene.path != "" &&
+                !mergeInProgress &&
+                GUILayout.Button("Start merging the open scene", GUILayout.Height(30)))
+            {
+                var manager = new MergeManagerScene(this, vcs);
+                if (manager.TryInitializeMerge())
+                {
+                    this.mergeManager = manager;
+                    CacheMergeActions();
+                }
+            }
+        }
+
+        private void DisplayPrefabMergeField()
+        {
+            if (!mergeInProgress)
+            {
+                var path = PathDetectingDragAndDropField("Drag a scene or prefab here to start merging", 80);
+                if (path != null)
+                {
+                    var asset = AssetDatabase.LoadAssetAtPath<Object>(path);
+                    
+                    if (IsPrefabAsset(asset))
+                    {
+                        var manager = new MergeManagerPrefab(this, vcs);
+                        if (manager.TryInitializeMerge(path))
+                        {
+                            this.mergeManager = manager;
+                            CacheMergeActions();
+                        }
+                    }
+                    else if (IsSceneAsset(asset))
+                    {
+                        var manager = new MergeManagerScene(this, vcs);
+                        if (manager.TryInitializeMerge(path))
+                        {
+                            this.mergeManager = manager;
+                            CacheMergeActions();
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool IsPrefabAsset(Object asset)
+        {
+            var assetType = asset.GetType();
+            return assetType == typeof(GameObject) ||
+                   assetType == typeof(DefaultAsset);
+        }
+
+        private static bool IsSceneAsset(Object asset)
+        {
+            var assetType = asset.GetType();
+            return assetType == typeof(SceneAsset);
+        }
+
+        private static string PathDetectingDragAndDropField(string text, float height)
+        {
+            var currentEvent = Event.current;
+
+            using (new GUIBackgroundColor(Color.black))
+            {
+                // Caching these sounds good on paper, but Unity tends to forget them randomly
+                var content = EditorGUIUtility.IconContent("RectMask2D Icon", string.Empty);
+                content.text = text;
+                
+                var buttonStyle = GUI.skin.GetStyle("Button");
+                var style = new GUIStyle(GUI.skin.GetStyle("Box"));
+                style.stretchWidth = true;
+                style.normal.background = buttonStyle.normal.background;
+                style.normal.textColor = buttonStyle.normal.textColor;
+                style.alignment = TextAnchor.MiddleCenter;
+                style.imagePosition = ImagePosition.ImageAbove;
+
+                GUILayout.Box(content, style, GUILayout.Height(height));
+            }
+            var rect = GUILayoutUtility.GetLastRect();
+
+            if (rect.Contains(currentEvent.mousePosition))
+            {
+                if (DragAndDrop.objectReferences.Length == 1)
+                {
+                    switch (currentEvent.type)
+                    {
+                        case EventType.DragUpdated:
+                            var asset = DragAndDrop.objectReferences[0];
+                            if (IsPrefabAsset(asset) || IsSceneAsset(asset))
+                            {
+                                DragAndDrop.visualMode = DragAndDropVisualMode.Move;
+                            }
+                            break;
+                        case EventType.DragPerform:
+                            var path = AssetDatabase.GetAssetPath(DragAndDrop.objectReferences[0]);
+                            DragAndDrop.AcceptDrag();
+                            return path;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Tab that offers various settings for the tool.
+        /// </summary>
+        private void OnGUISettingsTab()
+        {
+            var vcsPath = vcs.GetExePath();
+            var vcsPathNew = EditorGUILayout.TextField("Path to git.exe", vcsPath);
+            if (vcsPath != vcsPathNew)
+            {
+                vcs.SetPath(vcsPathNew);
+            }
+
+            automerge = DisplaySettingsToggle(automerge,
+                EDITOR_PREFS_AUTOMERGE,
+                "Automerge",
+                "(Automerge new/deleted GameObjects/Components upon merge start)");
+            
+            autofocus = DisplaySettingsToggle(autofocus,
+                EDITOR_PREFS_AUTOFOCUS,
+                "Auto Highlight",
+                "(Highlight GameObjects when applying a MergeAction to it)");
+        }
+
+        private static bool DisplaySettingsToggle(bool value, string editorPrefsKey, string title, string description)
+        {
+            var newValue = EditorGUILayout.Toggle(title, value);
+            if (value != newValue)
+            {
+                EditorPrefs.SetBool(editorPrefsKey, value);
+            }
+            GUILayout.Label(description);
+            return newValue;
+        }
+
+        /// <summary>
+        /// If no merge is in progress, draws the buttons to switch between tabs.
+        /// Otherwise, draws the "abort merge" button.
+        /// </summary>
+        private void DrawTabButtons()
+        {
+            if (!mergeInProgress)
+            {
+                string[] tabs = { "Merge", "Settings" };
+                tab = GUI.SelectionGrid(new Rect(72, 36, 300, 22), tab, tabs, 3);
+            }
+            else
+            {
+                GUI.backgroundColor = new Color(1, 0.4f, 0.4f, 1);
+                if (GUI.Button(new Rect(72, 36, 300, 22), "Abort merge"))
+                {
+                    mergeManager.AbortMerge();
+                    mergeManager = null;
+                }
+                GUI.backgroundColor = Color.white;
+            }
+        }
+
+        /// <summary>
+        /// Displays all MergeActions and the "apply merge" button if a merge is in progress.
+        /// </summary>
+        private void DisplayMergeProcess()
+        {
+            DrawCommandBar();
+
+            var done = DisplayMergeActions();
+            GUILayout.BeginHorizontal();
+            if (done && GUILayout.Button("Apply merge", GUILayout.Height(40)))
+            {
+                mergeManager.CompleteMerge();
+                mergeManager = null;
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        /// <summary>
+        /// Display extra commands to simplify merge process
+        /// </summary>
+        private void DrawCommandBar()
+        {
+            DrawQuickMergeSideSelectionCommands();
+            filterBar.Draw();
+        }
+
+        /// <summary>
+        /// Allow to select easily 'use ours' or 'use theirs' for all actions
+        /// </summary>
+        private void DrawQuickMergeSideSelectionCommands()
+        {
+            GUILayout.BeginHorizontal();
+            {
+                if (GUILayout.Button(new GUIContent("Use ours", "Use theirs for all. Do not apply merge automatically.")))
+                {
+                    mergeManager.allMergeActions.ForEach((action) => action.UseOurs());
+                }
+                if (GUILayout.Button(new GUIContent("Use theirs", "Use theirs for all. Do not apply merge automatically.")))
+                {
+                    mergeManager.allMergeActions.ForEach((action) => action.UseTheirs());
+                }
+                GUILayout.FlexibleSpace();
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        /// <summary>
+        /// Displays all GameObjectMergeActions.
+        /// </summary>
+        /// <returns>True, if all MergeActions are flagged as "merged".</returns>
+        private bool DisplayMergeActions()
+        {
+            var textColor = GUI.skin.label.normal.textColor;
+            GUI.skin.label.normal.textColor = Color.black;
+
+            bool done = true;
+
+            pageView.Draw(mergeActionsFiltered.Count, (index) =>
+            {
+                var actions = mergeActionsFiltered[index];
+                actions.OnGUI();
+                done = done && actions.merged;
+            });
+
+            GUI.skin.label.normal.textColor = textColor;
+            return done;
+        }
+
+        private void CacheMergeActions()
+        {
+            if (filter.useFilter)
+            {
+                mergeActionsFiltered = mergeManager.allMergeActions.Where((actions) => filter.IsPassingFilter(actions)).ToList();
+            }
+            else
+            {
+                mergeActionsFiltered = mergeManager.allMergeActions;
+            }
+        }
+    }
+}
---
+++ Assets/GitMerge/Editor/Utilities/GameObjectExtensions.cs
@@ -0,0 +1,110 @@
+﻿
+namespace GitMerge
+{
+    using System.Text;
+    using UnityEngine;
+    using UnityEditor;
+
+    public static class GameObjectExtensions
+    {
+        /// <summary>
+        /// Adds the copy of a Component to a GameObject.
+        /// </summary>
+        /// <param name="go">The GameObject that will get the new Component</param>
+        /// <param name="original">The original component to copy</param>
+        /// <returns>The reference to the newly added Component copy</returns>
+        public static Component AddComponent(this GameObject go, Component original)
+        {
+            var newComponent = go.AddComponent(original.GetType());
+
+            var originalProperty = new SerializedObject(original).GetIterator();
+            var newSerializedObject = new SerializedObject(newComponent);
+            var newProperty = newSerializedObject.GetIterator();
+
+            if (originalProperty.Next(true))
+            {
+                newProperty.Next(true);
+
+                while (originalProperty.NextVisible(true))
+                {
+                    newProperty.NextVisible(true);
+                    newProperty.SetValue(originalProperty.GetValue());
+                }
+            }
+
+            newSerializedObject.ApplyModifiedProperties();
+
+            return newComponent;
+        }
+
+        /// <summary>
+        /// Activates/deactivates the GameObjct, and hides it when it is disabled.
+        /// This is used for "their" objects to hide them while merging.
+        /// </summary>
+        /// <param name="go">The object do enable/disable</param>
+        /// <param name="active">Enable or disable the object?</param>
+        public static void SetActiveForMerging(this GameObject go, bool active)
+        {
+            go.SetActive(active);
+            go.hideFlags = active ? HideFlags.None : HideFlags.HideAndDontSave;
+        }
+
+        /// <summary>
+        /// Ping the GameObject in the hierarchy, select it, and center it in the scene view.
+        /// </summary>
+        /// <param name="go">The GameObject of interest</param>
+        public static void Highlight(this GameObject go)
+        {
+            // Focussing on the same object twice, zooms in to the coordinate instead of the bounding box.
+            if (Selection.activeObject == go) {
+                return;
+            }
+
+            Selection.activeGameObject = go;
+            EditorGUIUtility.PingObject(go);
+
+            var view = SceneView.lastActiveSceneView;
+            if (view)
+            {
+                view.FrameSelected();
+            }
+        }
+        
+        /// <summary>
+        /// Gets the path of this GameObject in the hierarchy.
+        /// </summary>
+        public static string GetPath(this GameObject gameObject)
+        {
+            var t = gameObject.transform;
+            var sb = new StringBuilder(RemovePostfix(t.name));
+            while (t.parent != null)
+            {
+                t = t.parent;
+                sb.Insert(0, RemovePostfix(t.name) + "/");
+            }
+            return sb.ToString();
+        }
+        
+        /// <summary>
+        /// Returns a child of this GameObject that has the same relative path to this GamObject as
+        /// the given GameObject has to it's root GameObject. 
+        /// </summary>
+        public static GameObject GetChildWithEqualPath(this GameObject gameObject, GameObject otherGameObject)
+        {
+            string fullHierarchyPath = otherGameObject.GetPath();
+            string relativeHierarchyPath = fullHierarchyPath.Substring(fullHierarchyPath.IndexOf("/", 1) + 1);
+            Transform result = gameObject.transform.Find(relativeHierarchyPath);
+            return result != null ? result.gameObject : gameObject; // fallback to root if a GameObject with equal path doesn't exist
+        }
+        
+        private static string RemovePostfix(string name)
+        {
+            if (name.EndsWith(MergeManagerBase.THEIR_FILE_POSTFIX))
+            {
+                return name.Substring(0, name.Length - MergeManagerBase.THEIR_FILE_POSTFIX.Length);
+            }
+
+            return name;
+        }
+    }
+}
