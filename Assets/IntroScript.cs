0877ac69842776bfab71fae8eb16cb00a2cbc027
Dudugs <duartesardao@sapo.pt>
2023-06-20 18:59:53 +0100

    separated skip from select and made esc skip too

--- Assets/IntroScript.cs
+++ Assets/IntroScript.cs
@@ -14,7 +14,7 @@ public class IntroScript : MonoBehaviour
     void Start()
     {
         actions.FindActionMap("menu interactions").Enable();
-        selectInput = actions.FindActionMap("menu interactions", true).FindAction("select", true);
+        selectInput = actions.FindActionMap("menu interactions", true).FindAction("skip", true);
 
     }
 
