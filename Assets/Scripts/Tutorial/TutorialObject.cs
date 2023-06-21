affcba96dffe4db808b5e73c0f4ade3bd35f26eb
Fab <fabiomiguelchenhuang@gmail.com>
2023-06-21 17:32:50 +0800

    feat: interaction with tutorial implemented

--- Assets/Scripts/Tutorial/TutorialObject.cs
+++ Assets/Scripts/Tutorial/TutorialObject.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public abstract class TutorialObject : ScriptableObject
 {
-    public Texture image;
+    public Sprite image;
     [TextArea(2, 20)]
     public string title;
     [TextArea(2, 20)]
--- Assets/TutorialScreen.cs
+++ Assets/TutorialScreen.cs
@@ -4,28 +4,89 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using UnityEngine.InputSystem;
 
 public class TutorialScreen : MonoBehaviour
 {
     public List<TutorialPages> tutorials = new List<TutorialPages>();
+    public int currentTutorialIndex;
+    public int currentPage;
 
+    public InputAction rightInput, leftInput;
+    public InputActionAsset actions;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTutorialIndex = 0;
+        currentPage = 0;
+
+        actions.FindActionMap("menu interactions").Enable();
+        rightInput = actions.FindActionMap("menu interactions", true).FindAction("moveRight", true);
+        leftInput = actions.FindActionMap("menu interactions", true).FindAction("moveLeft", true);
     }
+
+    void UpdateScreen()
+    {
+        GameObject.Find("Title").GetComponent<TextMeshProUGUI>().text = tutorials[currentTutorialIndex].tutorials[currentPage].title;
+        GameObject.Find("Description").GetComponent<TextMeshProUGUI>().text = tutorials[currentTutorialIndex].tutorials[currentPage].description;
+
+        GameObject.Find("ImageContainer").transform.Find("Image").GetComponent<Image>().sprite = tutorials[currentTutorialIndex].tutorials[currentPage].image;
+
+        UpdateArrows();
+
+    }
+
+    void UpdateArrows()
+    {
+        Color disableColor = new Color(65f / 255f, 65f / 255f, 65f / 255f, 1f);
+        Color activeColor = new Color(125f / 255f, 231f / 255f, 1f, 1f);
+
+
+
+        if (currentPage == 0)
+        {
+            GameObject.Find("LeftArrow").GetComponent<Image>().color = disableColor;
+        }
+        else
+        {
+            GameObject.Find("LeftArrow").GetComponent<Image>().color = activeColor;
+        }
+
+        if (currentPage < tutorials[currentTutorialIndex].tutorials.Count - 1)
+        {
+            GameObject.Find("RightArrow").GetComponent<Image>().color = activeColor;
+
+        }
+        else
+        {
+            GameObject.Find("RightArrow").GetComponent<Image>().color = disableColor;
+        }
+    }
+
+    
 
     // Update is called once per frame
     void Update()
     {
+        if (rightInput.WasPressedThisFrame() && (currentPage < tutorials[currentTutorialIndex].tutorials.Count-1) )
+        {
+            currentPage++;
+            UpdateScreen();
+            return;
+        }
+        if (leftInput.WasPressedThisFrame() && (currentPage > 0))
+        {
+            currentPage--;
+            UpdateScreen();
+            return;
+        }
     }
 }
 
 [Serializable]
 public class TutorialPages
 { 
-
     public string tutorialPageName;
     public List<TutorialObject> tutorials = new List<TutorialObject>();
 
